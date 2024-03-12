decd1a37b47a64d77a42ea63df61e9848fcd765c
sungwoo7180 <sungwoo7180@hanmail.net>
2024-03-12 23:52:39 +0900

    toolvar

--- js/toolbar_script.js
+++ js/toolbar_script.js
@@ -1,5 +1,17 @@
 function redirectToMain() {
-    window.location.href = 'toolbar.html';
+    window.location.href = 'homepage.html';
+}
+function redirectToMyPage() {
+    window.location.href = 'my_page.html';
+}
+function redirectToGoal() {
+    window.location.href = 'user_object.html';
+}
+function redirectToRecord() {
+    window.location.href = 'record_main.html';
+}
+function redirectToFeedback() {
+    window.location.href = 'feedback.html';
 }
 function logout() {
     window.location.href = 'login.html';
