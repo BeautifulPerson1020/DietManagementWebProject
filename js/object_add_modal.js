ec4e5015dbd53b5465d1fc5d1cb0e21a7a714721
saya61 <msaki061@gmail.com>
2024-03-14 14:34:56 +0900

    목표 모달 페이지 추가 및 데이터 추가에 따른 현재 목표 출력 + object_option.js 파일 수정 필요

--- js/object_add_modal.js
+++ js/object_add_modal.js
@@ -1,11 +1,29 @@
-const modal = document.querySelector('.object_modal');
 const btnOpenModal=document.querySelector('.btn-open-modal');
+const modalOn = document.getElementById('modalOn');
 
 btnOpenModal.addEventListener("click", ()=>{
-    modal.style.display="flex";
+    if (!isHaveContent) {
+        // 현재 목표가 이미 존재하는 경우 경고창 표시
+        alert("이미 목표가 존재합니다.");
+    }
+    else modalOn.classList.add('on');
 });
 
-const exModal = document.querySelector('.exit_object_modal');
+let isHaveContent=true;
+
+const exModal = document.querySelector('.exit-object-modal');
+const goalContent = document.getElementById('current-goal-content');
+const dDay = document.getElementById('dDay');
+const goal = document.getElementById('goal');
+const targetKcal = document.getElementById('targetKcal');
+const usedMoney = document.getElementById('usedMoney');
+
 exModal.addEventListener("click", () => {
     // 창 닫기
+    modalOn.classList.remove('on');
+    goalContent.innerText = dDay.value+'\n';
+    goalContent.innerText += goal.value+'\n';
+    goalContent.innerText += targetKcal.value+'\n';
+    goalContent.innerText += usedMoney.value;
+    isHaveContent = false;
 })
